aca391fdcfaab0e39b11439702f7ca057f267393
Andrew Hipp <andrew@havenagency.com>
2019-04-15 12:05:15 -0700

    fix: load pageheader props correctly

--- components/ProjectHeader/index.jsx
+++ components/ProjectHeader/index.jsx
@@ -2,7 +2,12 @@ import React from 'react';
 
 import css from './projectHeader.scss';
 
-const ProjectHeader = ({ name, client }) => (
+const ProjectHeader = ({
+    fields: {
+        name,
+        client,
+    } = {},
+}) => (
     <header className={css.projectHeader}>
         <h1 className={css.title}>{name}</h1>
         <h3 className={css.client}>{client}</h3>
