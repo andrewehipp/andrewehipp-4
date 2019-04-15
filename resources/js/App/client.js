7c1b17f51143bbc7a053db07153d2ac23af13e42
Andrew Hipp <andrew@havenagency.com>
2019-04-15 15:48:23 -0700

    chore: clean up

--- resources/js/App/client.js
+++
@@ -1,7 +0,0 @@
-const contentful = require('contentful');
-
-export default contentful.createClient({
-    space: 'fpic5f5rklmu',
-    environment: 'master', // defaults to 'master' if not set
-    accessToken: '4a038f56f28821d93ead9bfe9759bb1a7c6093eb8201bc7b93f7805e02e0338f',
-});
--- resources/js/main.js
+++
@@ -1,3 +0,0 @@
-import '../scss/global.scss';
-
-import './App';
