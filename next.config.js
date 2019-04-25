7062d4bfaba1ecb0c8e2e0d69d16bc8344c0ce26
Andrew Hipp <andrew@havenagency.com>
2019-04-25 13:34:23 -0700

    feat: integrate next-offline for service workers

--- next.config.js
+++ next.config.js
@@ -1,36 +1,39 @@
 const client = require('./client');
 const withSass = require('@zeit/next-sass');
+const withOffline = require('next-offline');
 
-module.exports = withSass({
-    cssModules: true,
-    cssLoaderOptions: {
-        localIdentName: '[folder]__[local]___[hash:base64:5]'
-    },
-    exportPathMap: async function(defaultPathMap) {
-        const projects = await client.getEntries({
-            content_type: 'project',
-        })
-            .then(res => res.items)
-            .then(res =>
-                res.reduce((acc, {
-                    fields: {
-                        slug,
-                    } = {},
-                }) => ({
-                    ...acc,
-                    [`/projects/${slug}`]: slug && {
-                        page: '/project',
-                        query: {
+module.exports = withOffline(
+    withSass({
+        cssModules: true,
+        cssLoaderOptions: {
+            localIdentName: '[folder]__[local]___[hash:base64:5]'
+        },
+        exportPathMap: async function(defaultPathMap) {
+            const projects = await client.getEntries({
+                content_type: 'project',
+            })
+                .then(res => res.items)
+                .then(res =>
+                    res.reduce((acc, {
+                        fields: {
                             slug,
-                        }
-                    },
-                }), {})
-            );
+                        } = {},
+                    }) => ({
+                        ...acc,
+                        [`/projects/${slug}`]: slug && {
+                            page: '/project',
+                            query: {
+                                slug,
+                            }
+                        },
+                    }), {})
+                );
 
-        return {
-          '/': { page: '/' },
-          '/archive': { page: '/archive' },
-          ...projects,
-        };
-    }
-});
+            return {
+              '/': { page: '/' },
+              '/archive': { page: '/archive' },
+              ...projects,
+            };
+        }
+    })
+);
