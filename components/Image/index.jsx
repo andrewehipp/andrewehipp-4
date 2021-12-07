48d5baecc6ddf87983ceff705b4fcb7f51a09a2f
Andrew Hipp <andrew@havenagency.com>
2021-12-07 11:39:03 -0800

    chore: add avif support

--- components/Image/index.jsx
+++ components/Image/index.jsx
@@ -5,13 +5,12 @@ const Image = ({
     alt = '',
     query,
     ...props
-}) => {
-    return (
-        <picture>
-            <source srcSet={`${src}?q=70&fm=webp${query ? `&${query}` : ''}`} type="image/webp" />
-            <img src={`${src}?q=70${query ? `&${query}` : ''}`} alt={alt} {...props} />
-        </picture>
-    );
-}
+}) => (
+    <picture>
+        <source srcSet={`${src}?q=70&fm=avif${query ? `&${query}` : ''}`} type="image/avif" />
+        <source srcSet={`${src}?q=70&fm=webp${query ? `&${query}` : ''}`} type="image/webp" />
+        <img src={`${src}?q=70${query ? `&${query}` : ''}`} alt={alt} {...props} />
+    </picture>
+);
 
 export default Image;
